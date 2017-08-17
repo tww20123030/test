cb646c0f59ba8dca6a236f94f75a29ff1425f92a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2017-08-17 02:45:23 +0000

    Deploy tww20123030/test to github.com/tww20123030/test.git:gh-pages

---
+++ Test.php
@@ -0,0 +1,11 @@
+<?php
+class Test{
+ public function testAutoPass(){
+    for($i=0;$i<100;$i++){
+    echo "$i\n";
+    }
+  }
+}
+$object1 = new Test();
+$object1->testAutoPass();
+?>
---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /home/tww/test/test && touch 1.txt
+git pull
+
